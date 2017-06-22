e1249dfdf5ec2183e93646acf7fa18665ad19f8b
victor <vilintritenmert@gmail.com>
2017-06-22 18:43:35 +0300

    init project

---
+++ database/migrations/2017_06_22_153239_users_update.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UsersUpdate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('picture');
+            $table->string('title');
+            $table->string('location');
+            $table->bigInteger('parent_id')->nullable();
+            $table->bigInteger('company_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name');
+            $table->dropColumn('first_name');
+            $table->dropColumn('last_name');
+            $table->dropColumn('picture');
+            $table->dropColumn('title');
+            $table->dropColumn('location');
+            $table->dropColumn('parent_id')->nullable();
+            $table->dropColumn('company_id');
+        });
+    }
+}
