1d27241088e2e75f9d4ae13a7106482575a57fb6
victor <vilintritenmert@gmail.com>
2017-07-01 13:39:06 +0300

    remove id from contacts create unique index in contacts

--- database/migrations/2017_07_01_093116_create_contacts.php
+++ database/migrations/2017_07_01_093116_create_contacts.php
@@ -14,9 +14,9 @@ class CreateContacts extends Migration
     public function up()
     {
         Schema::create('contacts', function (Blueprint $table) {
-            $table->increments('id');
             $table->integer('user_id');
             $table->string('data');
+            $table->unique(array('user_id', 'data'));
         });
     }
 
